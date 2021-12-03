c72749b20cf902c857d1bc53e46f62e548962750
Siva raman <shivasiva188@gmail.com>
2021-12-03 12:46:45 +0530

    NEW: project

---
+++ C#/EncapPrivate/EncapPrivate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapPrivate
+{
+    class Rectangle
+    {
+        private double length, width;
+
+        public void Acceptdetails()
+        {
+            Console.Write("Enter Length: ");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Width: ");
+            width = Convert.ToDouble(Console.ReadLine());
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+       class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Acceptdetails();
+            rect.Display();
+            Console.ReadLine();
+        }
+    }
+}
