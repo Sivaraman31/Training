e9dd719b34c5f410b896603209595c1d648cf4d0
Siva raman <shivasiva188@gmail.com>
2021-12-16 15:08:48 +0530

    Added: Program Armstrong Number

---
+++ C#/AmstrongNum/AmstrongNum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmstrongNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, r, sum = 0, temp = 0;
+            Console.WriteLine("Enter The Number: ");
+            n = int.Parse(Console.ReadLine());
+
+            temp = n;
+            while (n > 0)
+            {
+                r = n % 10;
+                sum=sum + (r * r * r);
+                n = n / 10;
+            }
+            if (temp==sum)
+            {
+                Console.WriteLine("Armstrong Number");
+            }
+            else 
+            { 
+                Console.WriteLine("Not Armstrong Number");
+            }
+        }
+    }
+}
