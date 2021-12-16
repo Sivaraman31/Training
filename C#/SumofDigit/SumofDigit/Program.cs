d2157166e616575601ebfce8271cc3227f506a31
Siva raman <shivasiva188@gmail.com>
2021-12-16 15:21:00 +0530

    ADD: SumofDigits

---
+++ C#/SumofDigit/SumofDigit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumofDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m, sum = 0;
+            Console.WriteLine("Enter a Number: ");
+            n = int.Parse(Console.ReadLine());
+            while (n > 0)
+            {
+                m = n % 10;
+                sum = sum + m;
+                n = n / 10;
+            }
+            Console.WriteLine("Sum is: " + sum);
+        }
+    }
+}
